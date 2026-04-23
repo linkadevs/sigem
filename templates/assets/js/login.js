2e9f5f22e21227c300206c729db799c2adff28b9
biamota599 <biamota599@gmail.com>
2026-04-23 02:57:47 -0300

    HTML completo

---
+++ templates/assets/js/login.js
@@ -0,0 +1,6 @@
+//quando a pessoa clicar no botão de entrar, deve ser redirecionada para a página de adm, cliente ou técnico dependendo do cpf.
+
+
+
+
+
--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,7 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Model\\' => array($baseDir . '/Model'),
     'Linkadevs\\Sigem\\' => array($baseDir . '/src'),
+    'Controller\\' => array($baseDir . '/Controller'),
 );
