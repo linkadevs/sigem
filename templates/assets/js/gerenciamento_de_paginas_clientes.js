4b32c6ac4d90e99684deaeddedfea158d8404df2
BeatrizMot4 <academico.beatriz.mota@gmail.com>
2026-04-24 03:14:23 -0300

    Fix: Correção da estrutura de redirecionamento em js

--- templates/assets/js/gerenciamento_de_paginas_clientes.js
+++ templates/assets/js/gerenciamento_de_paginas_clientes.js
@@ -1,21 +1,25 @@
-const voltar = document.querySelector('.voltar')
-const historico = document.querySelector('.historico')
-const  chamado = document.querySelector('.chamado')
+const voltar = document.querySelector('.voltar');
+const historicos = document.querySelectorAll('.historico');
+const chamados = document.querySelectorAll('.chamado');
 
 if (voltar) {
-    voltar.chamadoaddEventListener('click', () => {
+    voltar.addEventListener('click', () => {
         window.location.href = '';
     });
 }
 
-if (historico) {
-    historico.addEventListener('click', () => {
-        window.location.href = '';
-    });
-}
+historicos.forEach(historico => {
+    if (historico) {
+        historico.addEventListener('click', () => { 
+            window.location.href = '';
+        });
+    }
+});
 
-if (chamado) {
-    chamado.addEventListener('click', () => {
-        window.location.href = '';
-    });
-}
+chamados.forEach(chamado => {
+    if (chamado) {
+        chamado.addEventListener('click', () => {  
+            window.location.href = '';
+        });
+    }
+});
