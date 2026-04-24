bf481e0d336f335ca32d561b00cab31244d46f83
BeatrizMot4 <academico.beatriz.mota@gmail.com>
2026-04-24 04:52:15 -0300

    Fix: Limpeza do código

---
+++ templates/assets/js/gerenciamento_de_manutencoes_adm.js
@@ -0,0 +1,58 @@
+const perfil = document.querySelector('.btn_perfil');
+const maquinas = document.querySelector('.btn_maquinas');
+const clientes = document.querySelector('.btn_clientes');
+const chamados = document.querySelector('.btn_chamados');
+const manutencoes = document.querySelector('.btn_manutencoes');
+const pecas = document.querySelector('.btn_pecas');
+const tecnicos = document.querySelector('.btn_tecnicos');
+const logout = document.querySelector('.btn_logout');
+const historicos = document.querySelectorAll('.historico');
+
+
+if(perfil){
+perfil.addEventListener('click',() => {
+    window.location.href=''
+})
+}
+
+if(maquinas){
+    maquinas.addEventListener('click', ()=>{
+        window.location.href=''
+    })
+}
+
+if(clientes){
+    clientes.addEventListener('click', ()=>{
+        window.location.href = ''
+    })
+}
+
+if(chamados){
+    chamados.addEventListener('click', ()=>{
+        window.location.href=''
+    })
+}
+
+if(manutencoes){
+    manutencoes.addEventListener('clcik', () => {
+        window.location.href=''
+    })
+}
+
+if(pecas){
+    pecas.addEventListener('click',()=>{
+        window.location.href=''
+    })
+}
+
+if(tecnicos){
+    tecnicos.addEventListener('click', ()=>{
+        window.location.href=''
+    })
+}
+
+if(logout){
+    logout.addEventListener('click', ()=>{
+        window.location.href=''
+    })
+}
