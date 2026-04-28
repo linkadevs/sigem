abe60623eb4a7dea31fd252fbcc1bd031d6cda1a
BeatrizMot4 <academico.beatriz.mota@gmail.com>
2026-04-27 22:57:29 -0300

    Feat: Responsividade + JS para redirecionamento e visibilidade de senha

--- templates/assets/js/pagina_cadastro_tecnicos_administrador.js
+++ templates/assets/js/pagina_cadastro_tecnicos_administrador.js
@@ -1,1 +1,33 @@
+const olhofechado = document.querySelector('.olhofechado')
+const olhoaberto = document.querySelector('.olhoaberto')
+const voltar = document.querySelector('.voltar')
+const salvar = document.querySelector('.salvar')
+const camposenha = document.querySelector('#senha')
 
+
+if (voltar) {
+    voltar.addEventListener('click', () => {
+        window.location.href = ''
+    })
+}
+
+if (salvar) {
+    salvar.addEventListener('click', () => {
+        window.location.href = ''
+    })
+}
+
+function versenha() {
+    if (camposenha.type === 'password') {
+        camposenha.type = 'text'
+        olhofechado.style.display = 'none'
+        olhoaberto.style.display = 'block'
+    } else {
+        camposenha.type = 'password'
+        olhofechado.style.display = 'block'
+        olhoaberto.style.display = 'none'
+    }
+}
+
+olhofechado.addEventListener('click', versenha)
+olhoaberto.addEventListener('click', versenha)
