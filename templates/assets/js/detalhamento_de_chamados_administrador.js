f251b843f643e02f514a1cbfbdc9224f10c4e942
JanisaroBRR <fredalefsr@gmail.com>
2026-05-01 19:16:11 -0300

    Finalização do front-end da página

---
+++ templates/assets/js/detalhamento_de_chamados_administrador.js
@@ -0,0 +1,8 @@
+const botoes_menu = document.querySelectorAll('aside button');
+
+botoes_menu.forEach(botao => {
+    botao.addEventListener('click', () => {
+        // Lógica de redirecionamento futuro
+        // window.location.href = '';
+    });
+});
