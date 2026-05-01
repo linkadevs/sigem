91601d4fed4ccb84f5317c51ee8a1e7e36a47a78
JanisaroBRR <fredalefsr@gmail.com>
2026-05-01 19:46:16 -0300

    feat: Front-end completo da página

--- templates/assets/js/perfil_do_cliente.js
+++ templates/assets/js/perfil_do_cliente.js
@@ -1,16 +1,21 @@
 const btnVoltar = document.querySelector('.btn_voltar')
+const btnCancelar = document.querySelector('.btn_cancelar')
 const btnSalvar = document.querySelector('.btn_salvar')
 
 if(btnVoltar){
     btnVoltar.addEventListener('click', () => {
-        // Volta pra página anterior no histórico, mas você pode mudar pro window.location.href padrão
+        window.history.back()
+    })
+}
+
+if(btnCancelar){
+    btnCancelar.addEventListener('click', () => {
         window.history.back()
     })
 }
 
 if(btnSalvar){
     btnSalvar.addEventListener('click', () => {
-        // Coloque aqui o window.location.href da próxima página depois de salvar
-        console.log('Cliente salvo com sucesso')
+        window.location.href = ''
     })
 }
