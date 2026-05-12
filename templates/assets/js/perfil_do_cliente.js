265537e104eee2d26ae66b987f00ffc63e8f3d20
PedroEmanoel933 <pemanoel067@gmail.com>
2026-05-12 11:59:45 -0300

    feat: Adição do botão de visualizar senha

--- templates/assets/js/perfil_do_cliente.js
+++ templates/assets/js/perfil_do_cliente.js
@@ -19,3 +19,33 @@ if(btnSalvar){
         window.location.href = ''
     })
 }
+
+const olhoAberto = document.getElementById('olhoaberto')
+const olhoFechado = document.getElementById('olhofechado')
+const senhaAberta = document.getElementById('senha_desbloqueada')
+const senhaFechada = document.getElementById('senha_bloqueada')
+
+function alternarVisualizacao() {
+    if (senhaAberta.style.display === 'none') {
+
+        senhaAberta.style.display = 'block';
+        senhaFechada.style.display = 'none';
+        olhoAberto.style.display = 'none';
+        olhoFechado.style.display = 'block';
+    } else {
+
+        senhaAberta.style.display = 'none';
+        senhaFechada.style.display = 'block';
+        olhoAberto.style.display = 'block';
+        olhoFechado.style.display = 'none';
+    }
+}
+
+
+olhoAberto.addEventListener('click', () => {
+    alternarVisualizacao();
+});
+
+olhoFechado.addEventListener('click', () => {
+    alternarVisualizacao();
+});
